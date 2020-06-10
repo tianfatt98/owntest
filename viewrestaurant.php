5a97921c9c7a5f43a7b3ca577413e49ee7561171
tianfatt98 <66416155+tianfatt98@users.noreply.github.com>
2020-06-10 12:25:06 +0800

    Update viewrestaurant.php

--- viewrestaurant.php
+++ viewrestaurant.php
@@ -1,6 +1,6 @@
 <?php>
   // SQL Server Extension Sample Code:
-  $connectionInfo = array("UID" => "tianfatt98", "pwd" => "{Wishes_98}", "Database" => "DDAC_WebAppTutorial2", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+  $connectionInfo = array("UID" => "tianfatt98", "pwd" => "Wishes_98", "Database" => "DDAC_WebAppTutorial2", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
   $serverName = "tcp:webapp-db-tp042163.database.windows.net,1433";
   $conn = sqlsrv_connect($serverName, $connectionInfo);
   
