8559955c76629da79c4e226276d56d7be22c2ab9
tianfatt98 <66416155+tianfatt98@users.noreply.github.com>
2020-06-10 12:21:04 +0800

    Update viewrestaurant.php

--- viewrestaurant.php
+++ viewrestaurant.php
@@ -9,4 +9,23 @@
     die("Error connection: ".sqlsrv_errors());
   }
   echo "Connection to Db: Success!";
+
+//   $sql= "SELECT * FROM [dbo].[restaurant]";
+//   $getResults = sqlsrv_query($conn, $sql);
+//   if ($getResults == FALSE)
+//   {
+//     die(sqlsrv_errors());
+//   }
+//   echo "<table border = '1'>";
+//   while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
+//   {
+//   echo "<tr>";
+//   echo "<td>". $row['restaurant_id'] . "</td>";
+//   echo "<td>". $row['restaurant_name'] ."</td>";
+//   echo "<td>". $row['restaurant_address'] . "</td>";
+//   echo "<td>". $row['restaurant_phone'] . "</td>";
+//   echo "</tr>";
+//   }
+//   echo "</table>";
+//   sqlsrv_free_stmt($getResults);
 <?>
